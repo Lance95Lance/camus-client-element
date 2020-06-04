2045a0954eb63f41f40865404ea1431233439986
刘昌肇 <liuchangzhao@cathaylife.cn>
2020-06-04 14:58:53 +0800

    增加天数差计算

--- src/views/Tool/TimeExtrapolation.vue
+++ src/views/Tool/TimeExtrapolation.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="time-extrapolation">
     <div class="time-extrapolation">
-      <el-alert title="时间推算:进行时间推算，前几天后几天等" type="info" show-icon close-text="知道了"></el-alert>
+      <el-alert title="各种计算时间相关" type="info" :closable="false" ></el-alert>
 
       <el-form :inline="true" :model="dateInfo">
+        <el-alert title="时间推算:左包含右不包含" type="info" :closable="false" show-icon></el-alert>
+
         <el-form-item label="基础日期" required>
           <div class="block">
             <el-date-picker v-model="dateInfo.date" type="date" placeholder="选择基础日期"></el-date-picker>
@@ -21,34 +23,26 @@
         <div v-if="dateExtrapolationFlag === true">
           <el-alert :title="extrapolateDate" type="success" effect="dark" show-icon center></el-alert>
         </div>
+      </el-form>
+      <el-form :inline="true" :model="dateInterval">
+        <el-alert title="天数差计算" type="info" :closable="false" show-icon></el-alert>
 
-
-
-  <div class="block">
-    <span class="demonstration">开始日期</span>
-    <el-date-picker
-      v-model="dateInterval.begin_date"
-      type="date"
-      placeholder="选择日期">
-    </el-date-picker>
-  </div>
-  <div class="block">
-    <span class="demonstration">结束日期</span>
-    <el-date-picker
-      v-model="dateInterval.end_date"
-      align="right"
-      type="date"
-      placeholder="选择日期"
-      :picker-options="pickerOptions">
-    </el-date-picker>
-  </div>
-  <el-form-item required>
-    <el-button type="primary" @click="getDateInterval">计算</el-button>
-  </el-form-item>
-  <div v-if="IntervalDateFlag === true">
-    <el-alert :title="IntervalDate" type="success" effect="dark" show-icon center></el-alert>
-  </div>
-
+        <el-form-item label="开始日期" required>
+          <div class="block">
+            <el-date-picker v-model="dateInterval.begin_date" type="date" placeholder="选择日期"></el-date-picker>
+          </div>
+        </el-form-item>
+        <el-form-item label="结束" required>
+          <div class="block">
+            <el-date-picker v-model="dateInterval.end_date" type="date" placeholder="选择日期"></el-date-picker>
+          </div>
+        </el-form-item>
+        <el-form-item required>
+          <el-button type="primary" @click="getDateInterval">计算</el-button>
+        </el-form-item>
+        <div v-if="IntervalDateFlag === true">
+          <el-alert :title="IntervalDate" type="success" effect="dark" show-icon center></el-alert>
+        </div>
       </el-form>
     </div>
   </div>
@@ -69,16 +63,14 @@ export default {
         option: true,
         days: '',
       },
-      IntervalDateFlag:false,
-      IntervalDate:'',
+      IntervalDateFlag: false,
+      IntervalDate: '',
       dateInterval: {
         begin_date: '',
-        end_date:''
+        end_date: '',
       },
     };
   },
-
-
 
   async mounted() {},
   methods: {
@@ -107,27 +99,25 @@ export default {
         this.Notification('推算失败', result.message, 'warning');
       } else {
         this.dateExtrapolationFlag = true;
-        this.extrapolateDate = "推算后日期：" + result.data;
+        this.extrapolateDate = '推算后日期：' + result.data;
       }
     },
 
-
     async getDateInterval() {
+      this.IntervalDateFlag = false;
       const result = await apis.getDateInterval(
-        format(this.dateInterval.begin_date, 'YYYY-MM-DD'),format(this.dateInterval.end_date, 'YYYY-MM-DD')
+        format(this.dateInterval.begin_date, 'YYYY-MM-DD'),
+        format(this.dateInterval.end_date, 'YYYY-MM-DD')
       );
-        if (!result.success) {
+      if (!result.success) {
         this.Notification('计算失败', result.message, 'warning');
       } else {
         this.IntervalDateFlag = true;
-        this.IntervalDate = "计算天数为：" + result.data;
+        this.IntervalDate = '计算天数为：' + result.data;
       }
     },
-
-
   },
 };
-;
 </script>
 
 <style lang="less">
